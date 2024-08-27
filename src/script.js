1c017629ceabd20f4debde2bbba55c93b117ca88
gityashk <m.yashk6+github@gmail.com>
2024-08-28 00:54:30 +0530

    initial commit

---
+++ src/script.js
@@ -0,0 +1,12 @@
+function initApp() {
+    const hamburgerBtn = document.getElementById('hamburger-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+    function toggleMenu() {
+        mobileMenu.classList.toggle('hidden');
+        mobileMenu.classList.toggle('flex');
+    }
+    hamburgerBtn.addEventListener('click', toggleMenu);
+    mobileMenu.addEventListener('click', toggleMenu);
+}
+
+document.addEventListener('DOMContentLoaded', initApp);
